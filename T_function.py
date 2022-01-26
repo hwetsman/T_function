0e51a97b475e12478be8e39f07d731441e028c5d
hwetsman <howard@tocdr.com>
2022-01-25 21:42:32 -0600

    Code: create structure

--- T_function.py
+++ T_function.py
@@ -1,1 +1,15 @@
 # set up
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+# set up streamlit welcome
+# set up population of 330
+# set up streamlit selection of normal curve
+# set up streamlit selection of initial number to leave
+# set up production of normal curve
+# display normal curve
+# calculate steps of exit
+# display exit graph
