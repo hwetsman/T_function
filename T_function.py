59fa013d6882966cd20d974884f2e4d6dfe840d8
hwetsman <howard@tocdr.com>
2022-01-25 22:02:20 -0600

    Code: create distribution

--- T_function.py
+++ T_function.py
@@ -19,11 +19,16 @@ N = 330
 # select normal
 st.sidebar.radio('Distribution of T', ['Normal'])
 # set up streamlit selection of normal curve by input mean and sd
-st.sidebar.slider('Mean of Normal Distribution', min_value=10, max_value=100, value=50, step=1)
-st.sidebar.slider('sd of Normal Distribution', min_value=1, max_value=20, value=5, step=1)
+mean = st.sidebar.slider('Mean of Normal Distribution', min_value=10,
+                         max_value=100, value=50, step=1)
+sd = st.sidebar.slider('sd of Normal Distribution', min_value=1, max_value=20, value=5, step=1)
 # set up streamlit selection of initial number to leave
 st.sidebar.slider('Initial Number of People to Leave', min_value=1, max_value=5, value=1, step=1)
 # set up production of normal curve
+dist = np.random.normal(mean, sd, N)
+dist = [int(x) for x in dist]
+st.write(dist)
+
 # display normal curve
 # calculate steps of exit
 # display exit graph
