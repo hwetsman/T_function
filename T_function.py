579c93f159f8336b9aa51711bc1a8380be99e5f9
hwetsman <howard@tocdr.com>
2022-01-25 21:48:42 -0600

    Code: add welcome

--- T_function.py
+++ T_function.py
@@ -6,8 +6,16 @@ import streamlit as st
 
 
 # set up streamlit welcome
+st.write('Welcome to the Threshold Function simulator. The Threshold Function is \
+a theoretical construct in population science. The paradigm this simulator is based \
+on is the crowded theatre. Imagine a theatre of 330 people. Suddenly some number \
+of people get up and leave. Everyone sees them leave. Depending on some threshold, \
+T, which each individual has, each individual will react once their T of quickly \
+exiting people is reached. You may select the form of a normal function by which \
+T is distributed in the population and the original number of exiting people. The \
+script will plot the population exit. Later, other distributions will be added.')
 # set up population of 330
-# set up streamlit selection of normal curve
+# set up streamlit selection of normal curve by input mean and sd
 # set up streamlit selection of initial number to leave
 # set up production of normal curve
 # display normal curve
